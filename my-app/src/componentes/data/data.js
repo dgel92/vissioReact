645ccadf7a9fe252575c4acca8f0cf4fcc6d10b1
dgel92 <dgel92@gmail.com>
2022-10-18 20:37:07 -0300

    18-10-22

--- my-app/src/componentes/data/data.js
+++ my-app/src/componentes/data/data.js
@@ -1,36 +1,154 @@
 import alain from "../../multimedia/alain-1.jpg";
-import anoximenes from "../../multimedia/anoximenes-1.jpg";
+import alain1 from "../../multimedia/alain-2.jpg";
+import alain2 from "../../multimedia/alain-3.jpg";
+import alain3 from "../../multimedia/alain-4.jpg";
+import alain4 from "../../multimedia/alain-5.jpg";
+import alain5 from "../../multimedia/alain-6.jpg";
+
+
+import anoximenes from "../../multimedia/anoximenes-1.jpg";     
+import anoximenes1 from "../../multimedia/anoximenes-2.jpg";     
+import anoximenes2 from "../../multimedia/anoximenes-3.jpg";     
+import anoximenes3 from "../../multimedia/anoximenes-4.jpg";     
+import anoximenes4 from "../../multimedia/anoximenes-5.jpg";     
+
+
 import aristoteles from "../../multimedia/aristoteles-1.jpg";
+import aristoteles1 from "../../multimedia/aristoteles-2.jpg";
+
+
 import badiou from "../../multimedia/badiou-1.jpg";
+import badiou1 from "../../multimedia/badiou-2.jpg";
+import badiou2 from "../../multimedia/badiou-3.jpg";
+import badiou3 from "../../multimedia/badiou-4.jpg";
+import badiou4 from "../../multimedia/badiou-5.jpg";
+
+
 import barcelona from "../../multimedia/barcelona-1.jpg";
+import barcelona1 from "../../multimedia/barcelona-2.jpg";
+import barcelona2 from "../../multimedia/barcelona-3.jpg";
+import barcelona3 from "../../multimedia/barcelona-4.jpg";
+import barcelona4 from "../../multimedia/barcelona-5.jpg";
+
+
 import bimbaum from "../../multimedia/bimbaum-1.jpg";
+
+
 import bruno from "../../multimedia/bruno-1.jpg";
+import bruno1 from "../../multimedia/bruno-2.jpg";
+import bruno2 from "../../multimedia/bruno-3.jpg";
+
+
 import butler from "../../multimedia/butler-1.jpg";
+import butler1 from "../../multimedia/butler-2.jpg";
+
+
 import byung from "../../multimedia/byung-1.jpg";
+
+
 import charles from "../../multimedia/charles-1.jpg";
+import charles1 from "../../multimedia/charles-2.jpg";
+import charles2 from "../../multimedia/charles-3.jpg";
+import charles3 from "../../multimedia/charles-4.jpg";
+
+
 import ciceron from "../../multimedia/ciceron-1.jpg";
+import ciceron1 from "../../multimedia/ciceron-2.jpg";
+
+
 import copernico from "../../multimedia/copernico-1.jpg";
+import copernico1 from "../../multimedia/copernico-2.jpg";
+
+
 import dogones from "../../multimedia/dogones-1.jpg";
+import dogones1 from "../../multimedia/dogones-2.jpg";
+import dogones2 from "../../multimedia/dogones-3.jpg";
+import dogones3 from "../../multimedia/dogones-4.jpg";
+
+
 import francis from "../../multimedia/francis-1.jpg";
+import francis1 from "../../multimedia/francis-2.jpg";
+import francis2 from "../../multimedia/francis-3.jpg";
+
+
 import francisDos from "../../multimedia/francisDos-1.jpg";
+import francisDos1 from "../../multimedia/francisDos-2.jpg";
+
+
 import galileo from "../../multimedia/galileo-1.jpg";
+import galileo1 from "../../multimedia/galileo-2.jpg";
+
+
 import gayatri from "../../multimedia/gayatri-1.jpg";
+import gayatri1 from "../../multimedia/gayatri-2.jpg";
+import gayatri2 from "../../multimedia/gayatri-3.jpg";
+
+
 import george from "../../multimedia/george-1.jpg";
+import george1 from "../../multimedia/george-2.jpg";
+
+
 import habernas from "../../multimedia/habernas-1.jpg";
+import habernas1 from "../../multimedia/habernas-2.jpg";
+
+
 import hegel from "../../multimedia/hegel-1.jpg";
+import hegel1 from "../../multimedia/hegel-2.jpg";
+
+
 import heraclito from "../../multimedia/heraclito-1.jpg";
+import heraclito1 from "../../multimedia/heraclito-2.jpg";
+import heraclito2 from "../../multimedia/heraclito-3.jpg";
+
+
 import hobbes from "../../multimedia/hobbes-1.jpg";
+import hobbes1 from "../../multimedia/hobbes-2.jpg";
+import hobbes2 from "../../multimedia/hobbes-3.jpg";
+
+
+
 import inmanuel from "../../multimedia/inmanuel-1.jpg";
+import inmanuel1 from "../../multimedia/inmanuel-2.jpg";
+
+
 import jhonLocke from "../../multimedia/jhonLocke-1.jpg";
+import jhonLocke1 from "../../multimedia/jhonLocke-2.jpg";
+
+
 import judith from "../../multimedia/judith-1.jpg";
+import judith1 from "../../multimedia/judith-1.jpg";
+import judith2 from "../../multimedia/judith-1.jpg";
+import judith3 from "../../multimedia/judith-1.jpg";
+
+
 import jurgen from "../../multimedia/jurgen-1.jpg";
+import jurgen1 from "../../multimedia/jurgen-2.jpg";
+
+
 import maquiavelo from "../../multimedia/maquiavelo-1.jpg";
+import maquiavelo1 from "../../multimedia/maquiavelo-2.jpg";
+import maquiavelo2 from "../../multimedia/maquiavelo-3.jpg";
+
+
 import martha from "../../multimedia/martha-1.jpg";
+
+
 import michel from "../../multimedia/michel-1.jpg";
+
+
 import nasbaumm from "../../multimedia/nasbaumm-1.jpg";
+
+
 import noam from "../../multimedia/noam-1.jpg";
+
+
 import pitagora from "../../multimedia/pitagora-1.jpg";
+
+
 import platon from "../../multimedia/platon-1.jpg";
+import platon1 from "../../multimedia/platon-2.jpg";
+import platon2 from "../../multimedia/platon-3.jpg";
+import platon3 from "../../multimedia/platon-4.jpg";
 
 
 
@@ -39,6 +157,11 @@ const dataArray =[
     id: 1,
     nombre: 'Alain',
     img: alain,
+    img2: alain1,
+    img3: alain2,
+    img4: alain3,
+    img5: alain4,
+    img6: alain5,
     categoria: "escritorio",
     portada: true,
     stock: 5,
@@ -54,6 +177,10 @@ const dataArray =[
     id: 2,
     nombre: 'Anoximenes',
     img: anoximenes,
+    img1: anoximenes1,
+    img2: anoximenes2,
+    img3: anoximenes3,
+    img4: anoximenes4,
     categoria: "lampara de pie",
     portada: false,
     stock: 8,
@@ -69,6 +196,7 @@ const dataArray =[
     id: 3,
     nombre: 'Aristoteles',
     img: aristoteles,
+    img1: aristoteles1,
     categoria: "lampara de pie",
     portada: false,
     stock: 5,
@@ -84,6 +212,10 @@ const dataArray =[
     id: 4,
     nombre: 'Badiou',
     img: badiou,
+    img1: badiou1,
+    img2: badiou2,
+    img3: badiou3,
+    img4: badiou4,
     categoria: "escritorio",
     portada: false,
     stock: 8,
@@ -99,6 +231,10 @@ const dataArray =[
     id: 5,
     nombre: 'Barcelona',
     img: barcelona,
+    img1: barcelona1,
+    img2: barcelona2,
+    img3: barcelona3,
+    img4: barcelona4,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -129,6 +265,8 @@ const dataArray =[
     id: 7,
     nombre: 'bruno',
     img: bruno,
+    img1: bruno1,
+    img2: bruno2,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -144,6 +282,7 @@ const dataArray =[
     id: 8,
     nombre: 'Butler',
     img: butler,
+    img1: butler1,
     categoria: "escritorio",
     portada: true,
     stock: 18,
@@ -174,6 +313,9 @@ const dataArray =[
     id: 10,
     nombre: 'Charles',
     img: charles,
+    img1: charles1,
+    img2: charles2,
+    img3: charles3,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -189,6 +331,7 @@ const dataArray =[
     id: 11,
     nombre: 'Ciceron',
     img: ciceron,
+    img1: ciceron1,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -204,6 +347,7 @@ const dataArray =[
     id: 12,
     nombre: 'Copernico',
     img: copernico,
+    img1: copernico1,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -218,7 +362,10 @@ const dataArray =[
     {
     id: 13,
     nombre: 'Dogones',
-    img: dogones,
+    img: dogones,   
+    img1: dogones1,   
+    img2: dogones2,   
+    img3: dogones3,   
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -234,6 +381,8 @@ const dataArray =[
     id: 14,
     nombre: 'Francis',
     img: francis,
+    img1: francis1,
+    img2: francis2,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -249,6 +398,7 @@ const dataArray =[
     id: 15,
     nombre: 'FrancisDos',
     img: francisDos,
+    img1: francisDos1,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -264,6 +414,7 @@ const dataArray =[
     id: 16,
     nombre: 'Galileo',
     img: galileo,
+    img1: galileo1,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -279,6 +430,8 @@ const dataArray =[
     id: 17,
     nombre: 'Gayatri',
     img: gayatri,
+    img1: gayatri1,
+    img2: gayatri2,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -294,6 +447,7 @@ const dataArray =[
     id: 18,
     nombre: 'George',
     img: george,
+    img1: george1,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -309,6 +463,7 @@ const dataArray =[
     id: 19,
     nombre: 'Habernas',
     img: habernas,
+    img1: habernas1,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -324,6 +479,7 @@ const dataArray =[
     id: 20,
     nombre: 'Hegel',
     img: hegel,
+    img1: hegel1,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -339,6 +495,8 @@ const dataArray =[
     id: 21,
     nombre: 'Heraclito',
     img: heraclito,
+    img1: heraclito1,
+    img2: heraclito2,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -354,6 +512,8 @@ const dataArray =[
     id: 22,
     nombre: 'Hobbes',
     img: hobbes,
+    img1: hobbes1,
+    img2: hobbes2,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -369,6 +529,7 @@ const dataArray =[
     id: 23,
     nombre: 'Inmanuel',
     img: inmanuel,
+    img1: inmanuel1,
     categoria: "infantil",
     portada: false,
     stock: 18,
@@ -384,6 +545,7 @@ const dataArray =[
     id: 24,
     nombre: 'Jhon Locke',
     img: jhonLocke,
+    img1: jhonLocke1,
     categoria: "escritorio",
     portada: false,
     stock: 18,
@@ -399,6 +561,9 @@ const dataArray =[
     id: 25,
     nombre: 'Judith',
     img: judith,
+    img1: judith1,
+    img2: judith2,
+    img3: judith3,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -414,6 +579,7 @@ const dataArray =[
     id: 26,
     nombre: 'Jurgen',
     img: jurgen,
+    img1: jurgen1,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
@@ -429,6 +595,8 @@ const dataArray =[
     id: 27,
     nombre: 'Maquiavelo',
     img: maquiavelo,
+    img1: maquiavelo1,
+    img2: maquiavelo2,
     categoria: "lampara de pie",
     portada: true,
     stock: 18,
@@ -519,6 +687,9 @@ const dataArray =[
     id: 33,
     nombre: 'Platon',
     img: platon,
+    img1: platon1,
+    img2: platon2,
+    img3: platon3,
     categoria: "lampara de pie",
     portada: false,
     stock: 18,
