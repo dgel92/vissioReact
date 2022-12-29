86d06744dfdb57eac8d27de6eb689859c2943b17
dgel92 <dgel92@gmail.com>
2022-12-28 23:41:02 -0300

    22-12-22 23.40

--- my-app/src/componentes/Footer/Footer.jsx
+++ my-app/src/componentes/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer(){
             <div className="logoFooter">
                 <img src={Logo} className="logoFooterImg" alt="logo tienda"/>
             </div>
-            <div className="footer-text"><a>Siguenos</a></div>
+            <div className="footer-text"><a>Síguenos</a></div>
                 <div className="footerMenu">
                     <li><a href="https://wa.me/5493514597445"><Icon icon="logos:whatsapp-icon" height="45"/></a></li>
                     <li><a href="https://www.facebook.com/profile.php?id=100066980554240"><Icon icon="logos:facebook" height="40"/></a></li>
@@ -21,8 +21,8 @@ function Footer(){
             <hr></hr>
                 <div className="footer-text"><a>Importante</a></div>
                 <div className="footerlist">
-                    <li><Link to="/contactanos" className="links">Contactanos</Link></li>
-                    <li><Link to="/EnviosPedidosYDevoluciones" className="links">Pedidos, envios y devoluciones</Link></li>
+                    <li><Link to="/contactanos" className="links">Contáctanos</Link></li>
+                    <li><Link to="/EnviosPedidosYDevoluciones" className="links">Pedidos, envíos y devoluciones</Link></li>
                 </div>
             <br></br>
         </section>
